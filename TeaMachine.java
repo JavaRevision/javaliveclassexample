08313f1d43efe829b2a1e64447d450ce5f4e8aa4
march2299 <javabatchmar22@gmail.com>
2022-11-15 17:48:19 +0530

    program added by manasi Is-A and Has-A

--- TeaMachine.java
+++ TeaMachine.java
@@ -1,10 +1,31 @@
 package SampleApp;
-public class TeaMachine {
-    public static void main(String args[]){
-       Tea tea=new Tea();
-         tea.greenTea();
-
-       Milk milk=new Milk();
-        milk.hotMilk();
+class Drink{
+	public void refresherDrink(){
+		System.out.println("This is refresher");
+	}
+}
+class Tea extends Drink{
+    public void greenTea(){
+        System.out.println("This is GreenTea");
     }
 }
+
+ class Milk{
+    public void hotMilk(){
+        System.out.println(" It is a Milk");
+    }
+}
+
+public class TeaMachine {
+    public static void main(String args[]){
+		
+       Tea tea=new Tea();
+	   tea.refresherDrink();
+         tea.greenTea();
+		 
+       Milk milk=new Milk();
+        milk.hotMilk();
+		
+    }
+}
+// child extends parent === IS-A type
